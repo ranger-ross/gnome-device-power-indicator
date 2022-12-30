64720a15e5901e54094db2257d5138d1b76c5042
Ross <ross.sullivan101@gmail.com>
2022-12-30 10:48:14 +0900

    Added basic main loop

--- extension.js
+++ extension.js
@@ -1,16 +1,32 @@
-// Example #1
-
 const { St, Clutter } = imports.gi;
 const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
+const Mainloop = imports.mainloop;
+
+/**
+ * @typedef {Object} Device
+ * @property {string} model - The model name
+ * @property {boolean} rechargeable - Is the device rechargable
+ * @property {string} state - The power state of the device
+ * @property {string} percentage - The power percentage as a string ie. '100%'
+ * @property {string} iconName - The icon name
+ */
 
 let panelButton;
 
 const LOG_PREFIX = `[device-battery-indicator]`;
-const info = (msg) => log(`${LOG_PREFIX} INFO ${msg}`);
-const warn = (msg) => log(`${LOG_PREFIX} WARN ${msg}`);
-const debug = (msg) => log(`${LOG_PREFIX} DEBUG ${msg}`);
-const error = (msg) => log(`${LOG_PREFIX} ERROR ${msg}`);
+const logWithLevel = (level, msg) => log(`${new Date().toISOString()} ${LOG_PREFIX} ${level} ${msg}`);
+const info = (msg) => logWithLevel(`INFO`, msg);
+const warn = (msg) => logWithLevel(`WARN`, msg);
+const debug = (msg) => logWithLevel(`DEBUG`, msg);
+const error = (msg) => logWithLevel(`ERROR`, msg);
+
+let batteryCheckLoop = null;
+
+let panelButtonText = new St.Label({
+    text: "Hello World",
+    y_align: Clutter.ActorAlign.CENTER,
+});
 
 function init() {
 
@@ -20,43 +36,66 @@ function init() {
     panelButton = new St.Bin({
         style_class: "panel-button",
     });
-    let panelButtonText = new St.Label({
-        text: "Hello World",
-        y_align: Clutter.ActorAlign.CENTER,
-    });
+
     panelButton.set_child(panelButtonText);
 }
 
 function enable() {
+    info('Enabling extension');
 
-    info('RossEnable2');
-
-    getDevices();
+    batteryCheckLoop = Mainloop.timeout_add_seconds(2, () => {
+        try {
+            updateDeviceLoop();
+        } catch (e) {
+            error(e);
+        }
+        return true;
+    });
 
     // Add the button to the panel
     Main.panel._rightBox.insert_child_at_index(panelButton, 0);
 }
 
 function disable() {
-    info('RossDisable');
-    // Remove the added button from panel
+    info('Disabling extension');
+    if (batteryCheckLoop) {
+        Mainloop.source_remove(batteryCheckLoop);
+    }
     Main.panel._rightBox.remove_child(panelButton);
+}
+
+/**
+ * The primary update loop that checks the device state
+ */
+function updateDeviceLoop() {
+    info('Checking devices');
+    const devices = getDevices();
+
+    for (const device of devices) {
+
+        if (device?.model == 'G703 LIGHTSPEED Wireless Gaming Mouse w/ HERO') {
+            info(device.model + ' ' + device.percentage);
+
+            panelButtonText.text = device.model + ' ' + device.percentage;
+        }
+
+    }
+
 }
 
 function getDevices() {
     const deviceNames = getDeviceNames();
-
     const devices = [];
 
     for (const device of deviceNames) {
-        info(`Collecting data for device ${device}`);
+        debug(`Collecting data for device ${device}`);
 
         const rawDeviceInfo = getDeviceInfo(device);
         const deviceInfo = parseDeviceInfo(rawDeviceInfo);
 
-        info(`Device info => ${JSON.stringify(deviceInfo)}`)
+        debug(`Device info => ${JSON.stringify(deviceInfo)}`)
 
-        // TODO: Push device data to list
+        devices.push(deviceInfo);
     }
 
     return devices;
@@ -82,6 +121,11 @@ function isYes(property) {
     return getPropertyValue(property).includes('yes');
 }
 
+/**
+ * Convert the raw upower string output to a javascript object.
+ * 
+ * @returns {Device}
+ */
 function parseDeviceInfo(rawDeviceInfo) {
     info(`Parsing device info ` + rawDeviceInfo);
 
